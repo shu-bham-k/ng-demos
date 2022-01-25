746fe2271122dbb586f96903fa9b86fcd4c38088
shu-bham-k <shubham.k@codestoresolutions.com>
2022-01-25 17:42:31 +0530

    http module(25-01-2022)

---
+++ Angular/api/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ConfigService } from './config.service'; 
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit {
+  
+
+ data:any;
+
+  ngOnInit(): void {
+    
+this.postData.getPosts().subscribe((result: any)=> {
+  console.log("result",result)
+  this.data= result
+})
+
+  }
+  constructor (private postData:ConfigService) {}
+  
+}
